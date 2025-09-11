7e484121366545e59883869bf4f5d711369726fd
Henry Oduenyi <henryjr.oduenyi@gmail.com>
2025-09-11 08:57:17 +0200

    all files pls

---
+++ script.js
@@ -0,0 +1,25 @@
+const openButton = document.getElementById('open-sidebar');
+const navbar = document.getElementById('navbar');
+
+function openSidebar() {
+    navbar.classList.add('show');
+}
+
+function closeSidebar() {
+    navbar.classList.remove('show');
+}
+
+const media = window.matchMedia("(width < 800px)");
+
+
+
+
+const acc = document.querySelectorAll("p.accordion");
+
+    acc.forEach(item => {
+      item.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        panel.classList.toggle("show");
+      });
+    });
